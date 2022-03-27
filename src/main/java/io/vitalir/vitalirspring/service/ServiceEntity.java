5ef16bff40ec72c8e038d2e82034c80b5c98c28d
vitalir2 <vitalyi100@mail.ru>
2022-03-27 20:51:15 +0300

    Add first two spring tests

--- src/main/java/io/vitalir/vitalirspring/service/ServiceEntity.java
+++ src/main/java/io/vitalir/vitalirspring/service/ServiceEntity.java
@@ -1,18 +1,38 @@
 package io.vitalir.vitalirspring.service;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.Hibernate;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Objects;
 
 @Entity
 @Table(name = "services")
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class ServiceEntity {
     @Id
     @Column(name = "title")
     String title;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        ServiceEntity entity = (ServiceEntity) o;
+        return title != null && Objects.equals(title, entity.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
---
+++ src/test/java/io/vitalir/vitalirspring/service/ServiceControllerTest.java
@@ -0,0 +1,43 @@
+package io.vitalir.vitalirspring.service;
+
+import io.vitalir.vitalirspring.service.Service;
+import io.vitalir.vitalirspring.service.ServiceController;
+import io.vitalir.vitalirspring.service.ServicesService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ServiceController.class)
+public class ServiceControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ServicesService servicesService;
+
+    @Test
+    public void getServicesReturnsNotEmptyContent() throws Exception {
+        Service service = new Service("Health checking");
+        List<Service> services = List.of(service);
+        given(servicesService.getServices()).willReturn(services);
+
+        mockMvc.perform(get("/api/v1/services/")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].title", is(service.title())));
+    }
+}
