2c4363a3aadd11f1557abc0f78fd51ffc8723c16
vitalir2 <vitalyi100@mail.ru>
2022-04-12 18:59:54 +0300

    VSPRING-3: Fix csrf and old db scheme

--- src/main/java/io/vitalir/vitalirspring/security/SecurityWebConfig.java
+++ src/main/java/io/vitalir/vitalirspring/security/SecurityWebConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 
 @EnableWebSecurity
 @Configuration
@@ -13,11 +12,15 @@ public class SecurityWebConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .formLogin().disable()
+                .formLogin().and()
                 .authorizeHttpRequests( authorize ->
                         authorize
+                                .mvcMatchers("/login").permitAll()
                                 .mvcMatchers("/api/v1/services/**").permitAll()
                 )
-                .csrf(CsrfConfigurer::ignoringRequestMatchers);
+                .csrf(csrf -> csrf
+                        .ignoringAntMatchers("/login")
+                        .ignoringRequestMatchers(request -> request.getHeader("User-Agent").startsWith("Postman"))
+                );
     }
 }
