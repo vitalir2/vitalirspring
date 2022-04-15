a7bea6e8b8d6c8d06b435e8e745a78139709e4ec
vitalir2 <vitalyi100@mail.ru>
2022-04-15 10:21:22 +0300

    VSPRING-4: Add UserServiceImpl for getUserByEmail

--- src/main/java/io/vitalir/vitalirspring/features/user/UserServiceImpl.java
+++ src/main/java/io/vitalir/vitalirspring/features/user/UserServiceImpl.java
@@ -7,8 +7,14 @@ import java.util.Optional;
 @Service
 public class UserServiceImpl implements UserService {
 
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     public Optional<User> getUserByEmail(String email) {
-        return Optional.empty();
+        return userRepository.getUserByEmail(email);
     }
 }
---
+++ src/test/java/io/vitalir/vitalirspring/user/UserServiceImplTest.java
@@ -0,0 +1,52 @@
+package io.vitalir.vitalirspring.user;
+
+import io.vitalir.vitalirspring.features.user.User;
+import io.vitalir.vitalirspring.features.user.UserRepository;
+import io.vitalir.vitalirspring.features.user.UserService;
+import io.vitalir.vitalirspring.features.user.UserServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+public class UserServiceImplTest {
+
+    private static final String EMAIL = "g@gmail.com";
+    private static final String PASSWORD = "1234";
+    private static final User VALID_USER = new User(EMAIL, PASSWORD);
+
+    @Mock
+    private UserRepository userRepository;
+
+    private UserService userService;
+
+    @BeforeEach
+    public void init() {
+        userService = new UserServiceImpl(userRepository);
+    }
+
+    @Test
+    public void whenGetUserByEmailWhichExists_returnIt() {
+        given(userRepository.getUserByEmail(EMAIL)).willReturn(Optional.of(VALID_USER));
+
+        var result = userService.getUserByEmail(EMAIL);
+
+        assertThat(result).isEqualTo(Optional.of(VALID_USER));
+    }
+
+    @Test
+    public void whenGetUserByEmailWhichDoesNotExist_returnEmpty() {
+        given(userRepository.getUserByEmail(EMAIL)).willReturn(Optional.empty());
+
+        var result = userService.getUserByEmail(EMAIL);
+
+        assertThat(result).isEqualTo(Optional.empty());
+    }
+}
