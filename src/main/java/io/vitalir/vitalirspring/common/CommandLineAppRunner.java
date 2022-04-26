af46d9bd6e624f8911dc4995c2b925f60c38040c
vitalir2 <vitalyi100@mail.ru>
2022-04-26 17:53:38 +0300

    VSPRING-10: Add doctors prepopulation

--- src/main/java/io/vitalir/vitalirspring/common/CommandLineAppRunner.java
+++ src/main/java/io/vitalir/vitalirspring/common/CommandLineAppRunner.java
@@ -1,11 +1,17 @@
 package io.vitalir.vitalirspring.common;
 
+import io.vitalir.vitalirspring.features.doctors.domain.Doctor;
+import io.vitalir.vitalirspring.features.doctors.domain.DoctorRepository;
+import io.vitalir.vitalirspring.features.doctors.domain.MedicalSpecialty;
 import io.vitalir.vitalirspring.features.user.domain.model.Role;
 import io.vitalir.vitalirspring.features.user.domain.model.User;
 import io.vitalir.vitalirspring.features.user.domain.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Set;
 
 @Component
 public class CommandLineAppRunner implements CommandLineRunner {
@@ -14,11 +20,15 @@ public class CommandLineAppRunner implements CommandLineRunner {
     private static final String DEFAULT_PASSWORD = "password";
 
     private final UserRepository userRepository;
+
     private final PasswordEncoder passwordEncoder;
 
-    public CommandLineAppRunner(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    private final DoctorRepository doctorRepository;
+
+    public CommandLineAppRunner(UserRepository userRepository, PasswordEncoder passwordEncoder, DoctorRepository doctorRepository) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.doctorRepository = doctorRepository;
     }
 
     @Override
@@ -29,6 +39,7 @@ public class CommandLineAppRunner implements CommandLineRunner {
         }
         String password = parsePassword(args);
         addAdmin(password);
+        preInitDoctors();
     }
 
     private String parsePassword(String... args) {
@@ -48,5 +59,64 @@ public class CommandLineAppRunner implements CommandLineRunner {
                 Role.ADMIN
         );
         userRepository.save(admin);
+    }
+
+    private void preInitDoctors() {
+        for (var doctor: createInitialDoctors()) {
+            doctorRepository.save(doctor);
+        }
+    }
+
+    private static List<Doctor> createInitialDoctors() {
+        var doctor1 = new Doctor();
+        doctor1.setName("Henry Peterson");
+        doctor1.setExperienceYears(10);
+        doctor1.setMedicalSpecialties(Set.of(MedicalSpecialty.CARDIOLOGY));
+
+        var doctor2 = new Doctor();
+        doctor2.setName("Natalia Sherikhova");
+        doctor2.setExperienceYears(13);
+        doctor2.setMedicalSpecialties(Set.of(MedicalSpecialty.ENDOCRINOLOGY));
+
+        var doctor3 = new Doctor();
+        doctor3.setName("Julia Peterson");
+        doctor3.setExperienceYears(4);
+        doctor3.setMedicalSpecialties(Set.of(MedicalSpecialty.PULMONOLOGY));
+
+        var doctor4 = new Doctor();
+        doctor4.setName("Peter Barinov");
+        doctor4.setExperienceYears(8);
+        doctor4.setMedicalSpecialties(Set.of(MedicalSpecialty.GASTROENTEROLOGY));
+
+        var doctor5 = new Doctor();
+        doctor5.setName("Mark Wertov");
+        doctor5.setExperienceYears(30);
+        doctor5.setMedicalSpecialties(Set.of(MedicalSpecialty.ONCOLOGY, MedicalSpecialty.RHEUMATOLOGY));
+
+        var doctor6 = new Doctor();
+        doctor6.setName("George Narinov");
+        doctor6.setExperienceYears(8);
+        doctor6.setMedicalSpecialties(Set.of(MedicalSpecialty.PEDIATRICS));
+
+        var doctor7 = new Doctor();
+        doctor7.setName("Vitaly Vostov");
+        doctor7.setExperienceYears(5);
+        doctor7.setMedicalSpecialties(Set.of(MedicalSpecialty.GASTROENTEROLOGY));
+
+        var doctor8 = new Doctor();
+        doctor8.setName("Anastasia Orekhova");
+        doctor8.setExperienceYears(16);
+        doctor8.setMedicalSpecialties(Set.of(MedicalSpecialty.NEPHROLOGY));
+
+        return List.of(
+                doctor1,
+                doctor2,
+                doctor3,
+                doctor4,
+                doctor5,
+                doctor6,
+                doctor7,
+                doctor8
+        );
     }
 }
