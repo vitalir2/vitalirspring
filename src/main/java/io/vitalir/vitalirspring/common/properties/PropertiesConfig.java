423fe5a4690193e4e1e75875f071fcb2ff570c77
vitalir2 <vitalyi100@mail.ru>
2022-05-02 18:53:36 +0300

    VSPRING-13: Add new Dev config

--- src/main/java/io/vitalir/vitalirspring/common/properties/PropertiesConfig.java
+++ src/main/java/io/vitalir/vitalirspring/common/properties/PropertiesConfig.java
@@ -1,6 +1,5 @@
 package io.vitalir.vitalirspring.common.properties;
 
-import io.vitalir.vitalirspring.common.properties.PropertiesSecurityConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
